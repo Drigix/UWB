0629bb9d799865acfe5d608f257569d154572889
drigix <michrarr@gmail.com>
2023-07-15 13:51:08 +0200

    Create necessary modules and make configuration for modules

---
+++ uwb-client-app/src/app/components/components.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { PrimeNGLibsModule } from '@shared/primeng-libs.module';
+
+
+@NgModule({
+  imports: [
+    PrimeNGLibsModule
+  ],
+  exports: [],
+  declarations: [],
+  providers: [],
+})
+export class ComponentsModule { }
