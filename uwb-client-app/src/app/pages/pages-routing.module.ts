7a4893320b5fa2322fb8ca38637ba67c152cfe5a
drigix <michrarr@gmail.com>
2023-07-15 16:10:48 +0200

    Add routing configuration to project

---
+++ uwb-client-app/src/app/pages/pages-routing.module.ts
@@ -0,0 +1,17 @@
+import { authGuard } from './../config/user-route-access.service';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(
+      [
+        {
+          path: '',
+          loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+         },
+      ]
+    )
+  ]
+})
+export class PagesRoutingModule { }
