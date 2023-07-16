06423b4dc10406e7805f2d35be3d201f77a838bb
drigix <michrarr@gmail.com>
2023-07-16 19:28:32 +0200

    Add custom spinner

---
+++ uwb-client-app/src/app/shared/uwb-spinner/uwb-spinner.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'uwb-spinner',
+  templateUrl: './uwb-spinner.component.html',
+  styleUrls: ['./uwb-spinner.component.scss']
+})
+
+export class UwbSpinnerComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
