36aa058be681fcd0b3644c4acf6c444e292dd110
drigix <michrarr@gmail.com>
2023-07-17 21:25:35 +0200

    Test created form and calendar

--- uwb-client-app/src/app/pages/user/user.component.ts
+++ uwb-client-app/src/app/pages/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -8,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
 
   date = new Date();
-
-  constructor() { }
+  formGroup?: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.formGroup = this.formBuilder.group({
+      name: [{value: null, disabled: false}, [Validators.required]],
+      surname: [{value: null, disabled: false}, [Validators.required]],
+      date: [{value: null, disabled: false}, [Validators.required]]
+    });
   }
 
 }
