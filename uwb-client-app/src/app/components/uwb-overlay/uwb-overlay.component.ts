81fa4597d8c1bb9fc82431e6036343ea47a892fe
drigix <michrarr@gmail.com>
2023-07-22 21:11:53 +0200

    Create overlay component

---
+++ uwb-client-app/src/app/components/uwb-overlay/uwb-overlay.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'uwb-overlay',
+  templateUrl: './uwb-overlay.component.html'
+})
+
+export class UwbOverlayComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
