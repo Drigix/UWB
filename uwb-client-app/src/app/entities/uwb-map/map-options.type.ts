36673eaed076b19cbcc4fe7893c9bfc8a67cdf9c
drigix <michrarr@gmail.com>
2023-08-26 11:50:27 +0200

    Create enums and types for map component

---
+++ uwb-client-app/src/app/entities/uwb-map/map-options.type.ts
@@ -0,0 +1,11 @@
+export enum MapPointType {
+  ANCHOR = 'anchor',
+  TAG = 'tag'
+};
+
+export enum MapOverlayType {
+  ANCHOR = 'anchor',
+  TAG = 'tag'
+};
+
+export type MapDisplayType = 'normal' | 'mapWithButtons' | 'mapWithBodyInside';
