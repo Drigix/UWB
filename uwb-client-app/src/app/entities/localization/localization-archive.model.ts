faaf029195dcd28bcd0f1c844fbe969b64f81f2e
drigix <michrarr@gmail.com>
2023-08-26 16:43:16 +0200

    Create service and model to localizations archive data

---
+++ uwb-client-app/src/app/entities/localization/localization-archive.model.ts
@@ -0,0 +1,8 @@
+export interface ILocalizationArchive {
+  id?: number;
+  x?: number;
+  y?: number;
+  z?: number;
+  date?: Date;
+  objectId?: number;
+}
---
+++ uwb-client-app/src/app/services/localizations/localizations-archive.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class LocalizationsArchiveService {
+
+  private resourceUrl = '../../../assets/data/data-heatmap-points.json';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  findAll(): Observable<any> {
+    return this.http.get(this.resourceUrl);
+  }
+}
