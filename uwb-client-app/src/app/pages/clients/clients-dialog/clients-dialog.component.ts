9549e7f35536160e2f9c2c80aff3454f65970dc7
drigix <michrarr@gmail.com>
2023-09-13 20:35:46 +0200

    Connect organization units page with all request in client application

--- uwb-client-app/src/app/pages/clients/clients-dialog/clients-dialog.component.ts
+++ uwb-client-app/src/app/pages/clients/clients-dialog/clients-dialog.component.ts
@@ -1,8 +1,12 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IClientUnit } from '@entities/client/client-unit.model';
-import { IClient } from '@entities/client/client.model';
+import { IClient, NewClient } from '@entities/client/client.model';
+import { TranslateService } from '@ngx-translate/core';
 import { ClientUnitsService } from '@services/clients/client-units.service';
+import { ClientsService } from '@services/clients/clients.service';
+import { ToastService } from '@shared/toast/toast.service';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 
 @Component({
@@ -16,13 +20,16 @@ export class ClientsDialogComponent implements OnInit {
   treeSelectItems: IClientUnit[] = [];
   edit = false;
   selectedClient?: IClient;
-  selectedParentClient?: IClientUnit;
+  selectedParentClient?: IClient;
 
   constructor(
     private formBuilder: FormBuilder,
     private ref: DynamicDialogRef,
     private config: DynamicDialogConfig,
-    private clientUnitsService: ClientUnitsService
+    private clientsService: ClientsService,
+    private clientUnitsService: ClientUnitsService,
+    private toastService: ToastService,
+    private translateService: TranslateService
   ) { }
 
   ngOnInit() {
@@ -39,34 +46,73 @@ export class ClientsDialogComponent implements OnInit {
   loadFormGroup(): void {
     this.formGroup = this.formBuilder.group({
       name: [{value: null, disabled: false}, [Validators.required]],
-      orgUnit: [{value: null, disabled: false}, [Validators.required]]
+      parentOrganizationUnitId: [{value: null, disabled: false}, [Validators.required]],
+      treePath: [{value: null, disabled: false}]
     });
     if(this.edit) {
       this.formGroup.patchValue({
         name: this.selectedClient?.name,
-        orgUnit: null
+        parentOrganizationUnit: this.selectedClient
       });
     }
   }
 
   loadClients(): void {
-    this.clientUnitsService.findAll().subscribe(
-      (res) => {
-        this.treeSelectItems = res;
+    this.clientsService.findTree().subscribe(
+      (res: HttpResponse<IClientUnit[]>) => {
+        this.treeSelectItems = res.body ?? [];
         if(this.edit) {
-          this.selectedParentClient = this.clientUnitsService.findByClientId(this.treeSelectItems[0], this.selectedClient?.parentOrgUnit!)!;
-          this.formGroup?.get('orgUnit')?.patchValue(this.selectedParentClient);
+          const currentOrganizationUnit = this.clientUnitsService.findByClientId(this.treeSelectItems[0], this.selectedClient?.parentOrganizationUnitId!);
+          this.formGroup?.get('parentOrganizationUnitId')?.setValue(currentOrganizationUnit);
+          this.onParentOrganizationUnitSelect(currentOrganizationUnit!.data);
+        } else if(this.selectedClient) {
+          const currentOrganizationUnit = this.clientUnitsService.findByClientId(this.treeSelectItems[0], this.selectedClient?.id!);
+          this.formGroup?.get('parentOrganizationUnitId')?.setValue(currentOrganizationUnit);
+          this.onParentOrganizationUnitSelect(currentOrganizationUnit!.data);
         }
       }
     );
   }
 
-  onSave(): void {
-    const value = this.formGroup?.getRawValue();
+  onParentOrganizationUnitSelect(organizationUnit: IClient): void {
+    this.selectedParentClient = organizationUnit;
+    this.formGroup?.get('treePath')?.setValue(this.selectedParentClient.treePath! + this.selectedParentClient.id?.toString() + ';');
   }
 
-  onCloseDialog(): void {
-    console.log('close');
-    this.ref.close();
+  onSave(): void {
+    if(this.edit) {
+      const value: IClient = this.formGroup?.getRawValue();
+      value.parentOrganizationUnitId = this.selectedParentClient?.id;
+      value.id = this.selectedClient?.id;
+      this.clientsService.update(value).subscribe(
+        {
+          next:() => {
+            this.toastService.showSuccessToast({summary: this.translateService.instant('global.toast.header.success'), detail: this.translateService.instant('client.dialog.editSuccess')});
+            this.onCloseDialog(true);
+          },
+          error:() => {
+            this.toastService.showErrorToast({summary: this.translateService.instant('global.toast.header.error'), detail: this.translateService.instant('client.dialog.editError')});
+          }
+        }
+      );
+    } else {
+      const value: NewClient = this.formGroup?.getRawValue();
+      value.parentOrganizationUnitId = this.selectedParentClient?.id;
+      this.clientsService.save(value).subscribe(
+        {
+          next:() => {
+            this.toastService.showSuccessToast({summary: this.translateService.instant('global.toast.header.success'), detail: this.translateService.instant('client.dialog.addSuccess')});
+            this.onCloseDialog(true);
+          },
+          error:() => {
+            this.toastService.showErrorToast({summary: this.translateService.instant('global.toast.header.error'), detail: this.translateService.instant('client.dialog.addError')});
+          }
+        }
+      );
+    }
+  }
+
+  onCloseDialog(response = false): void {
+    this.ref.close(response);
   }
 }
--- uwb-client-app/src/app/services/clients/clients.service.ts
+++ uwb-client-app/src/app/services/clients/clients.service.ts
@@ -1,20 +1,26 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { API_URL } from '@config/api-url.constans';
+import { IClientUnit } from '@entities/client/client-unit.model';
 import { IClient, NewClient } from '@entities/client/client.model';
-import { ClientResponseType } from '@entities/global/httpresponse-types.model';
-import { Observable } from 'rxjs';
+import { ClientArrayResponseType, ClientResponseType, ClientUnitArrayResponseType, ClientUnitResponseType } from '@entities/global/httpresponse-types.model';
+import { Observable, map } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class ClientsService {
 
-  private resourceUrl = '../../../assets/data/data-clients.json';
+  private resourceUrl = API_URL + 'organization-unit';
 
   constructor(
     private http: HttpClient
   ) { }
 
-  findAll(): Observable<any> {
-    return this.http.get(this.resourceUrl);
+  findAll(): Observable<ClientArrayResponseType> {
+    return this.http.get<IClient[]>(this.resourceUrl, { observe: 'response' });
+  }
+
+  findTree(): Observable<ClientUnitArrayResponseType> {
+    return this.http.get<IClientUnit[]>(`${this.resourceUrl}/tree`, { observe: 'response' });
   }
 
   findById(id: number): Observable<ClientResponseType> {
@@ -29,7 +35,9 @@ export class ClientsService {
     return this.http.put(this.resourceUrl, client, { observe: 'response' });
   }
 
-  delete(id: number): Observable<any> {
-    return this.http.get(`${this.resourceUrl}/${id}`, { observe: 'response' });
+  delete(id: number): Observable<string> {
+    return this.http.delete(`${this.resourceUrl}/${id}`, { observe: 'response', responseType: 'text' }).pipe(
+      map(response => response.body as string)
+    );
   }
 }
