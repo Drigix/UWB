38de5546b2ee91850d88fcaf0c31b5a6b58f07b8
drigix <michrarr@gmail.com>
2023-10-21 10:15:19 +0200

    Configure jwt authentication and cors in gateway

--- uwb-client-app/src/app/config/api-url.constans.ts
+++ uwb-client-app/src/app/config/api-url.constans.ts
@@ -1,3 +1,3 @@
-export const API_URL = 'http://localhost:8081/api/uwb/';
-export const API_NOTIFICATION_URL = 'http://localhost:8082/api/uwb-notification/';
+export const API_URL = 'http://localhost:8080/api/uwb/';
+export const API_NOTIFICATION_URL = 'http://localhost:8080/api/uwb-notification/';
 
